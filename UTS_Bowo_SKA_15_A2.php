e30cb05ca48819c5898cd6ed32e7e65e676ce528
MochamadArief17 <mochamadariefwibowo034@gmail.com>
2017-05-29 13:19:39 +0700

    Add files via upload

---
+++ UTS_Bowo_SKA_15_A2.php
@@ -0,0 +1,29 @@
+<html>
+<title>UTS_Mochamad Arief Wibowo(SKA-15.A.2)</title>
+<form action="proses.php" method="get">
+<body>
+<div id="header"><h2>PENDAFTARAN PELANGGAN BARU</h2></div>
+<tr>
+<td id="P"><b>DATA PRIBADI Bowo</b></td><br>
+Nama Lengkap    : <input type="text" name="nama" /><br>
+Jenis Kelamin   : <input tytpe="text" name="jk" /><br>
+E-mail          : <input tytpe="text" name="email" /><br><br>
+
+<td id="P"><b>DATA ALAMAT PENGIRIM</b></td><br>
+Nama Negara 	: <input type="text" name="negara" /><br>
+Kota			: <input type="text" name="kota" /><br>
+Alamat			: <input type="text" name="alamat" /><br>
+Kode Pos		: <input type="text" name="kodepos" /><br><br>
+
+<td id="P"><b>DATA NOMOR KONTAK</b></td><br>
+Nomor Telpon	: <input type="text" name="telpon" /><br>
+Nomor Handphone	: <input type="text" name="hp" /><br>
+Nomor Faksimili	: <input type="text" name="faks" /><br><br>
+
+</pre>
+		<input type="submit" value="Submit">
+		<input type="reset" value="Reset">
+
+</form>
+</body>
+</html>
